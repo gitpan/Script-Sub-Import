4cf03f4ef199d8953d90be57dc5e84b0c0e5544b
Yuki Kimoto <kimoto.yuki@gmail.com>
2009-12-05 11:19:08 +0000

    Import of KIMOTO/Script-Sub-Import-0.0201 from CPAN.
    gitpan-cpan-distribution: Script-Sub-Import
    gitpan-cpan-version:      0.0201
    gitpan-cpan-path:         KIMOTO/Script-Sub-Import-0.0201.tar.gz
    gitpan-cpan-author:       KIMOTO
    gitpan-cpan-maturity:     released

--- lib/Script/Sub/Import.pm
+++ lib/Script/Sub/Import.pm
@@ -18,7 +18,7 @@ sub import {
     
     # Join die
     my $message = 'Script::Sub::Import die';
-    $script = "package $package;\ndie '$message';\n" . $script;
+    $script = "package $package;\n" . $script . "BEGIN{die '$message'}\n";
     
     # Eval
     eval $script;
@@ -38,11 +38,11 @@ Script::Sub::Import - Import only subroutines from perl script
 
 =head1 VERSION
 
-Version 0.0101
+Version 0.0201
 
 =cut
 
-our $VERSION = '0.0101';
+our $VERSION = '0.0201';
 
 
 =head1 SYNOPSIS
@@ -56,6 +56,18 @@ our $VERSION = '0.0101';
 'Script::Sub::Import' can import only subroutine in perl scrit;
 
 This is very useful at automatic test because you can test only subroutine.
+
+If you want to import subroutines to main package, do this.
+
+    use Script::Sub::Import 'script.pl';
+
+If you want to import subroutines to specified package, do this.
+
+    use Script::Sub::Import 'script.pl' => 'Some::Package';
+    
+This module can be used from command line.
+
+    perl -MScript::Sub::Import=script.pl basic.t
 
 =head1 AUTHOR
 
